29c7a053f5429196808744ca4422c88af19b50d0
yumcyawiz <hashdolf1923@gmail.com>
2021-12-19 18:44:14 +0900

    test water cornell box

--- examples/main.cpp
+++ examples/main.cpp
@@ -5,29 +5,37 @@
 int main() {
   const uint32_t width = 512;
   const uint32_t height = 512;
-  const uint32_t n_iterations = 10;
-  const uint32_t n_photons = 100000;
+  const uint32_t n_iterations = 100;
+  const uint32_t n_photons = 200000;
   const float alpha = 3.0f / 4.0f;
-  const float initial_radius = 0.01f;
+  const float initial_radius_surface = 0.01f;
+  const float initial_radius_volume = 5.0f * initial_radius_surface;
   const uint32_t max_depth = 100;
 
   Image image(width, height);
 
-  const auto camera = std::make_shared<ThinLensCamera>(
-      Vec3f(0, 1, 6), Vec3f(0, 0, -1), 0.38f * PI, 32.0f);
-  camera->focus(Vec3f(-0.2496, -0.001, 0.6));
+  const Vec3f camera_position = Vec3f(0, 1, 6);
+  const Vec3f lookat = Vec3f(0, 1, 0);
+  const Vec3f forward = normalize(lookat - camera_position);
+  const auto camera =
+      std::make_shared<PinholeCamera>(camera_position, forward, 0.25 * PI);
+  // const auto camera = std::make_shared<ThinLensCamera>(camera_position,
+  // forward,
+  //                                                      0.38f * PI, 64.0f);
+  // camera->focus(Vec3f(0, 1, 0.5));
 
   // build scene
   Scene scene;
-  scene.loadObj("cornellbox-water2.obj");
+  scene.loadObj(
+      "CornellBox-Water-Small-Light-Covered-Cube-Floor-No-Backwall.obj");
   scene.build();
 
   // render
   UniformSampler sampler;
-  // PathTracing integrator(camera, 1000);
+  // PathTracing integrator(camera, 100);
   // integrator.render(scene, sampler, image);
-  PPM integrator(camera, n_iterations, n_photons, alpha, initial_radius,
-                 max_depth);
+  PPM integrator(camera, n_iterations, n_photons, alpha, initial_radius_surface,
+                 initial_radius_volume, max_depth);
   integrator.render(scene, sampler, image);
 
   // gamma correction
